a92e0a5500860c77fae91fd12016ba06878b53c5
Wolfgang cr. Ko <crazia@gmail.com>
2012-12-17 11:13:07 +0900

    게시판 형식으로 변경

--- app.js
+++ app.js
@@ -9,8 +9,17 @@ var express = require('express')
   , http = require('http')
   , path = require('path');
 
+var fs = require ('fs');
+
+// redis 설정해 주는 부분 
+var RedisStore = require ('connect-redis')(express);
+var redis = require('redis');
+
 var ArticleProvider = require('./articleprovider-mongodb').ArticleProvider;
 //var ArticleProvider = require('./articleprovider-memory').ArticleProvider;
+// redis 클라이언트 실제로 세팅해주는 부분 
+
+// var redisClient = redis.createClient( 6379 , 'grr');
 
 var app = express();
 
@@ -20,7 +29,19 @@ app.configure(function(){
   app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
+	              
+  /// session 설정시 필요한 것들 
+//  app.use(express.cookieParser());	              
   app.use(express.bodyParser());
+	              
+  // session 을 redis 를 이용해서 저장한다고 설정 하는 부분
+  // app.use(express.session({
+	//   secret: 'session fo mongo',
+	//   store: new RedisStore({ 
+	// 		  client: redisClient
+	// 		  })
+	//   }));
+	              
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(require('stylus').middleware(__dirname + '/public'));
@@ -29,11 +50,20 @@ app.configure(function(){
 
 app.configure('development', function(){
   app.use(express.errorHandler());
+	              
+  // source 가 예쁘게 출력되게 할려는 옵션 
+  app.locals.pretty = true;
 });
 
 // app.get('/', routes.index);
 
- var articleProvider = new ArticleProvider('192.168.0.87', 27017);
+
+// mongodb 주소가 필요합니다. 
+// 보통은 localhost 인데 제 경우는 osx 에 SSD 를 사용하고 있는 중이라 
+// 몽고디비를 도저히 제꺼에 설치할 수가 없더군요. 
+// grr 는 버추얼 머신에 띄워놓은 우분투 서버 입니다. 
+
+ var articleProvider = new ArticleProvider('grr', 27017);
 // var articleProvider = new ArticleProvider();
 
 app.get('/', function(req,res) {
@@ -81,6 +111,90 @@ app.post('/blog/addComment', function (req, res) {
       res.redirect('/blog/' + req.param('_id'));
     });
 });
+
+
+app.post ('/upload', function (req, res){
+	//console.log(req);
+	console.log ('CKEditor:' + req.param('CKEditor'));
+	console.log ('CKEditorFuncNum:' + req.param('CKEditorFuncNum'));
+	console.log ('langCode:' + req.param('langCode'));
+	          
+	console.log (req.files.upload.path);
+	console.log (req.files.upload.name);
+	          
+	var save_url ;
+
+	var newPath = __dirname + "/public/images/crazia" ; // 만약 login 이 되기 시작하면 crazia 대신 개인의 ID 로 처리할 수도 있다. 	 
+	// save_url = newPath + '/' + req.files.upload.name ;
+	save_url = "/images/crazia/" + req.files.upload.name ;
+	          
+ 	// fs.rename (req.files.upload.path, save_url , function (err){
+	// 	if (err) {
+			
+	// 		console.log (err);
+	// 		var regex = /(\w+).(\w+)/;
+	// 		var match = regex.exec(req.files.upload.name);
+	// 		save_url = newPath + '/' + match[1] + '_1.' + match[2] ;
+	// 		fs.rename (req.files.upload.path , save_url , function (err){
+	// 			res.render('upload.jade',{ 
+	// 				funcNum: req.param ('CKEditorFuncNum'),
+	// 				save_url: save_url
+	// 			});
+
+	// 		} );
+	// 	}else {
+	// 		res.render('upload.jade',{ 
+	// 			funcNum: req.param ('CKEditorFuncNum'),
+	// 			save_url: save_url
+	// 		});
+			
+	// 	}
+
+	// 	// res.render("<script>window.parent.CKEDITOR.tools.callFunction(" +
+	// 	//          req.param('CKEditorFuncNum') + "," + save_url + ", '업로드완료');</script>");
+	// 	console.log ('upload complete');
+	// });
+	          
+ 	fs.readFile(req.files.upload.path, function (err, data) {
+    fs.writeFile(newPath + "/" + req.files.upload.name , data, function (err) {
+	    if (err) {
+		    
+		    console.log (err);
+		    var regex = /(\w+).(\w+)/;
+		    var match = regex.exec(req.files.upload.name);
+		    save_url = newPath + '/' + match[1] + '_1.' + match[2] ;
+		    fs.writeFile (save_url , data , function (err){
+			    res.render('upload.jade',{ 
+				    funcNum: req.param ('CKEditorFuncNum'),
+				    save_url: save_url
+			    });
+		    });
+		    
+		    
+	    } else {
+		    
+			    console.log (save_url);
+		    
+		    res.send("<script>window.parent.CKEDITOR.tools.callFunction(" +
+		         req.param('CKEditorFuncNum') + ", \"" + save_url + "\");</script>");
+
+		    //res.send("<script> alert(\'hello\');</script>");
+		    
+			    // res.render('upload.jade',{ 
+				  //   funcNum: req.param ('CKEditorFuncNum'),
+				  //   save_url: save_url
+			    // });
+		    
+	    }
+	               
+	               
+    });
+  });
+	          
+});
+
+
+
 
 app.get('/users', user.list);
 
