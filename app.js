949c3eb38b1a07949b6752267c6dea2327ee9b59
Wolfgang cr. Ko <crazia@gmail.com>
2014-01-28 19:08:29 +0900

    bug fixed for localhost

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ var express = require('express')
 
 var fs = require ('fs');
 
-// redis 설정해 주는 부분 
+// redis 설정해 주는 부분
 var RedisStore = require ('connect-redis')(express);
 var redis = require('redis');
 
 var ArticleProvider = require('./articleprovider-mongodb').ArticleProvider;
 //var ArticleProvider = require('./articleprovider-memory').ArticleProvider;
-// redis 클라이언트 실제로 세팅해주는 부분 
+// redis 클라이언트 실제로 세팅해주는 부분
 
 // var redisClient = redis.createClient( 6379 , 'grr');
 
@@ -30,19 +30,19 @@ app.configure(function(){
   app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
-	              
-  /// session 설정시 필요한 것들 
-//  app.use(express.cookieParser());	              
+
+  /// session 설정시 필요한 것들
+//  app.use(express.cookieParser());
   app.use(express.bodyParser());
-	              
+
   // session 을 redis 를 이용해서 저장한다고 설정 하는 부분
   // app.use(express.session({
 	//   secret: 'session fo mongo',
-	//   store: new RedisStore({ 
+	//   store: new RedisStore({
 	// 		  client: redisClient
 	// 		  })
 	//   }));
-	              
+
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(require('stylus').middleware(__dirname + '/public'));
@@ -51,20 +51,20 @@ app.configure(function(){
 
 app.configure('development', function(){
   app.use(express.errorHandler());
-	              
-  // source 가 예쁘게 출력되게 할려는 옵션 
+
+  // source 가 예쁘게 출력되게 할려는 옵션
   app.locals.pretty = true;
 });
 
 // app.get('/', routes.index);
 
 
-// mongodb 주소가 필요합니다. 
-// 보통은 localhost 인데 제 경우는 osx 에 SSD 를 사용하고 있는 중이라 
-// 몽고디비를 도저히 제꺼에 설치할 수가 없더군요. 
-// grr 는 버추얼 머신에 띄워놓은 우분투 서버 입니다. 
+// mongodb 주소가 필요합니다.
+// 보통은 localhost 인데 제 경우는 osx 에 SSD 를 사용하고 있는 중이라
+// 몽고디비를 도저히 제꺼에 설치할 수가 없더군요.
+// grr 는 버추얼 머신에 띄워놓은 우분투 서버 입니다.
 
- var articleProvider = new ArticleProvider('grr', 27017);
+ var articleProvider = new ArticleProvider('localhost', 27017);
 // var articleProvider = new ArticleProvider();
 
 app.get ('/', blog.index);
@@ -72,10 +72,11 @@ app.get ('/blog/new', blog.new);
 app.get('/blog/:id', blog.show);
 app.post('/blog/addComment', blog.addComment);
 app.post ('/upload', blog.post_upload);
+app.post('/blog/new', blog.save);
 
-// test code 
+// test code
 app.get('/users', user.list);
-var socket_name = __dirname + "/comjuck.sock" ; // 만약 login 이 되기 시작하면 crazia 대신 개인의 ID 로 처리할 수도 있다. 	 
+var socket_name = __dirname + "/comjuck.sock" ; // 만약 login 이 되기 시작하면 crazia 대신 개인의 ID 로 처리할 수도 있다.
 
 // http.createServer(app).listen(socket_name, function(){
 //   console.log("Express server listening on port " + app.get('port'));
--- routes/blog.js
+++ routes/blog.js
@@ -1,6 +1,6 @@
 var ArticleProvider = require('../articleprovider-mongodb').ArticleProvider;
 
-var articleProvider = new ArticleProvider('grr', 27017);
+var articleProvider = new ArticleProvider('localhost', 27017);
 
 
 exports.index = function(req, res){
@@ -16,10 +16,20 @@ exports.new = function(req, res){
 	res.render('blog_new.jade', {title: 'Neo Post'});
 };
 
+exports.save = function(req, res) {
+    articleProvider.save({
+        title: req.param('title'),
+        body: req.param('body')
+    }, function( error, docs) {
+      res.redirect('/');
+    });
+};
+
+
 exports.show = function(req, res){
 	console.log("req.params.id is " + req.params.id);
 	articleProvider.findById(req.params.id, function(error, article){
-			       
+
 	console.log("request is complete " +  article.title);
       // res.send(article);
 	res.render('blog_show.jade',{
@@ -47,64 +57,64 @@ exports.post_upload = function(req, res){
 	console.log ('CKEditor:' + req.param('CKEditor'));
 	console.log ('CKEditorFuncNum:' + req.param('CKEditorFuncNum'));
 	console.log ('langCode:' + req.param('langCode'));
-	          
+
 	console.log (req.files.upload.path);
 	console.log (req.files.upload.name);
-	          
+
 	var save_url ;
 
-	var newPath = __dirname + "/public/images/crazia" ; // 만약 login 이 되기 시작하면 crazia 대신 개인의 ID 로 처리할 수도 있다. 	 
+	var newPath = __dirname + "/public/images/crazia" ; // 만약 login 이 되기 시작하면 crazia 대신 개인의 ID 로 처리할 수도 있다.
 
 	fs.stat (newPath , function (err, stats) {
 		if (err) {
-			
+
 			fs.mkdirSync (__dirname + "/public/images");
-			fs.mkdirSync(newPath); // 지금은 여기에 임시로 만들어 두지만 나중에 회원 가입 하면 이미지 저장하는 디렉토리를 따로 만들어야 할 듯 		          	          
-			
+			fs.mkdirSync(newPath); // 지금은 여기에 임시로 만들어 두지만 나중에 회원 가입 하면 이미지 저장하는 디렉토리를 따로 만들어야 할 듯
+
 		}
-		
+
 		save_url = "/images/crazia/" + req.files.upload.name ;
-		         
-		// 이미 존재하는지 검사 하는 루틴 
+
+		// 이미 존재하는지 검사 하는 루틴
 
 		fs.stat (newPath + "/" + req.files.upload.name , function (err , stats){
 			if (err) {
-				// 없다면 없으니 맘 편하게 쓰기 
+				// 없다면 없으니 맘 편하게 쓰기
 			  fs.readFile(req.files.upload.path , function (err, data) {
 				  fs.writeFile(newPath + "/" + req.files.upload.name , data , function (err) {
 
 					  res.send("<script>window.parent.CKEDITOR.tools.callFunction(" +
 		         req.param('CKEditorFuncNum') + ", \"" + save_url + "\");</script>");
-					               
+
 				  });// end of fs.writeFile
 			  }); // end of readFile
-				
-				
+
+
 			} else {
-				// 만약 존재한다면 쓸 파일 이름 변경 시켜야 함 
+				// 만약 존재한다면 쓸 파일 이름 변경 시켜야 함
 				var regex = /(\w+).(\w+)/;
 				var match = regex.exec(req.files.upload.name);
-				save_url = "/images/crazia/" + match[1] + '_1.' + match[2] ; 
+				save_url = "/images/crazia/" + match[1] + '_1.' + match[2] ;
 
-								
+
 			  fs.readFile(req.files.upload.path , function (err, data) {
 				  fs.writeFile(newPath + "/" + match[1] + '_1.' + match[2] , data , function (err) {
 
 					  res.send("<script>window.parent.CKEDITOR.tools.callFunction(" +
 		         req.param('CKEditorFuncNum') + ", \"" + save_url + "\");</script>");
-					               
+
 				  });// end of fs.writeFile
 			  }); // end of readFile
-				
+
 			}// end of else
 		}); // end of fs.stat
-		         
+
 	});
 
-	// 이 부분은 나중에 따로 빼야 한다. Sync 방식을 썼기 때문에 한시적으로 여기에 둔 것임 
-	// 굳이 여기에 둔 이유는 images 디렉토리를 안 만들어 주고 이 소스를 사용할 시에 예상 되는 에러를 피하기 위해서임 
+	// 이 부분은 나중에 따로 빼야 한다. Sync 방식을 썼기 때문에 한시적으로 여기에 둔 것임
+	// 굳이 여기에 둔 이유는 images 디렉토리를 안 만들어 주고 이 소스를 사용할 시에 예상 되는 에러를 피하기 위해서임
 	// fs.mkdirSync (__dirname + "/public/images");
-	// fs.mkdirSync(newPath); // 지금은 여기에 임시로 만들어 두지만 나중에 회원 가입 하면 이미지 저장하는 디렉토리를 따로 만들어야 할 듯 		          	          
+	// fs.mkdirSync(newPath); // 지금은 여기에 임시로 만들어 두지만 나중에 회원 가입 하면 이미지 저장하는 디렉토리를 따로 만들어야 할 듯
 
 	// save_url = newPath + '/' + req.files.upload.name ;
 
